565f082d3be9b1d5018edc7793303c54435156dd
scalesrzn <scalesrzn@gmail.com>
2021-03-17 05:38:42 +0300

    Добавил плавную прокрутку до самого конца исходя из текущей высоты документа

--- ParseMyAudioVK.js
+++ ParseMyAudioVK.js
@@ -1,57 +1,49 @@
 var oSong,
     oArtist,
     iCountSong,
-    iWindowWidth,
-    iOldWidth,
-    iNewWidth,
+    iWindowHeight,
+    iOldHeight,
     aMusic;
-
-async function ScrollPage(Width){
-    let promise = new Promise((resolve, reject) => {
-        setTimeout(() => window.scrollTo(document.body.scrollWidth, Width), 30000);
-      });
-    await promise;
+// Скроллим страницу
+async function ScrollPage(Height){
+    let scroll = new Promise((resolve, reject) => {
+        window.scrollTo({top: Height ,behavior: "smooth"});
+        // window.scrollTo(Height, document.body.scrollWidth); //резкая прокрутка по координатам
+       });
+    await scroll;
+    
 }
-//Получаем высоту документа
-function GetMaxWidth(){
-    iWindowWidth    = Math.max  (
+// Получаем высоту документа
+function GetMaxHeight(){
+    iWindowHeight    = Math.max  (
                                     document.body.scrollHeight, document.documentElement.scrollHeight,
                                     document.body.offsetHeight, document.documentElement.offsetHeight,
                                     document.body.clientHeight, document.documentElement.clientHeight
                                 )
-    return iWindowWidth
+    return iWindowHeight
 }
 
-//Получаем список всех аудио
+// Получаем список всех аудио
 function GetAudioList(){
     oArtist     =   document.getElementsByClassName('audio_row__performers');
     oSong       =   document.getElementsByClassName('audio_row__title_inner');
     iCountSong  =   oSong.length;
     for (var i = 0; i < iCountSong; i++){
-        aMusic+= (oArtist[i].textContent.toString() + " - " +  oSong[i].textContent.toString() + "\r\n");
+        aMusic+= (oArtist[i].textContent.toString() + " - " +  oSong[i].textContent.toString() + ";" + "\r\n");
     }
     return aMusic
 }
-    
-function GetPageStatus(OldWidth){
-    if (OldWidth < GetMaxWidth())
-        return true
-    else
-        return false
-
-}
 
 function Main(){
-    iOldWidth   =   GetMaxWidth();
-    // alert(iOldWidth);
-    window.scrollTo(document.body.scrollWidth, 100000);
-    // alert(GetMaxWidth());
-    while (GetPageStatus(iOldWidth)){
-        iOldWidth   = GetMaxWidth();
-        ScrollPage(100000);
-        // alert(iOldWidth);
+    iOldHeight   = GetMaxHeight()  // эта переменная и (см ниже)
+    alert(iOldHeight)
+    ScrollPage(iOldHeight);
+    alert(GetMaxHeight())
+    while (iOldHeight < GetMaxHeight()){ // значение ТЕКУЩЕГО GetMaxHeight() совпадает. Скролл выполняется после, хоят стоит await
+        iOldHeight   = GetMaxHeight();
+        ScrollPage(iOldHeight);
     }
-    console.log(GetAudioList());
+    // console.log(GetAudioList());
 }
 
 Main();
